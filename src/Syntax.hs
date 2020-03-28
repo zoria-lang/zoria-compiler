048572660a3ca78a38df19d935568a6dfb8225b9
Jakub Grobelny <jgrobelny1@gmail.com>
2020-03-28 04:12:45 +0100

    Renamed Program module to Syntax.
    Changed the name of Program.hs to Syntax.hs.
    Added more expression types.
    Updated .gitignore.

--- src/Syntax.hs
+++ src/Syntax.hs
@@ -1,4 +1,4 @@
-module Program where
+module Syntax where
 
 import qualified Data.Text as T
 import Text.Megaparsec (SourcePos)
@@ -25,16 +25,28 @@ data Module = Module
     , modulePath    :: FilePath
     , moduleImports :: [Import]
     , moduleExports :: Maybe [Located Name]
-    , moduleDefs    :: [LetDef]
+    , moduleDefs    :: [TopLevelDef]
     }
 
 data Import = Import
-    { source      :: Module
-    , importLoc   :: SourcePos
-    , importNames :: Maybe [Located Name]
+    { source    :: Module
+    , importLoc :: SourcePos
+    , importIds :: Maybe [Located Name]
     }
 
-type LetDef = Int
+data TopLevelDef
+    = TopLevelLet LetDef
+    | AliasDef    TypeAlias
+    | TypeDef     TypeDecl
+    | ClassDef    Class
+    | InstanceDef Instance
+
+data LetDef = LetDef
+    { letPattern :: Pattern
+    , letTypeSig :: Maybe TypeSig
+    , letExpr    :: Expr
+    , letLoc     :: SourcePos
+    }
 
 data Expr 
     = IntLit Int SourcePos
@@ -42,10 +54,23 @@ data Expr
     | FloatLit Double SourcePos
     | StringLit T.Text SourcePos
     | BoolLit Bool SourcePos
-    | Unit SourcePos
+    | UnitLit SourcePos
+    | Var Name SourcePos
     | And Expr Expr SourcePos
     | Or Expr Expr SourcePos
     | If Expr Expr Expr SourcePos
+    | Block [Expr] SourcePos
+    | LetIn LetDef Expr
+    | MultiLet [LetDef] Expr
+    | Lambda Pattern Expr SourcePos
+    | Tuple [Expr] SourcePos
+    | List [Expr] SourcePos
+    | Array [Expr] SourcePos
+    | App Expr Expr
+    | Match PatternMatching
+    | Extern Name Name SourcePos
+    | Internal Name SourcePos
+    | AnnotatedExpr Expr TypeSig SourcePos
 
 instance Functor Located where
     fmap f (Located loc a) = Located loc $ f a
