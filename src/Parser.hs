8569adb37fdf8e6a80ec4b78f2020ec28169b7d3
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-02 03:22:44 +0200

    Created the Lexer module.
    I have decided that having a separate lexer will make
    writing the parser easier (e.g. no need to skip whitespace
    manually) so I've created a separate Lexer module that
    will deal with lexing and token definitions.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,1 +1,15 @@
-module Parser where
+module Parser 
+    () 
+where
+
+import Lexer
+import qualified Data.Text as T
+import Text.Megaparsec (Parsec, ParsecT, )
+import Data.Void (Void)
+
+
+type IOParser = ParsecT Void T.Text IO
+
+type Parser = Parsec Void T.Text
+
+
