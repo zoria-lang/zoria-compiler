eb83496aa6b7cb0121e573dc703dc883c5e76a99
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-04 04:14:03 +0200

    Defined the Parser monad.
    Since our grammar is tricky to parse, we needed the following features:
    - reading new files during parsing
    - passing and updating a dictionary of custom operators
    
    To achieve that, I used monad transformers to create the parser type
    that is able to both perfom stateful computations and side effects.
    
    The Parser type wraps the IOState type using ParsecT monad transformer.
    The IOState is the IO monad wrapped inside StateT transformer.

--- src/Parser.hs
+++ src/Parser.hs
@@ -5,22 +5,21 @@ module Parser
 where
 
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as L
+import Control.Monad.State (StateT, runStateT)
 import Data.Void (Void)
-import Control.Monad (void)
+import Control.Monad (void, mapM_)
+import Control.Monad.IO.Class (liftIO)
+import Utility (Position(..))
 
 
-type IOParser = P.ParsecT Void T.Text IO
 
-type Parser = P.Parsec Void T.Text
-
-data Position = Position
-    { posOffset :: !Int
-    , posFile   :: FilePath
-    }
-  deriving Show
+type ParserState = Int -- TODO: change
+type IOState = StateT ParserState IO
+type Parser = P.ParsecT Void T.Text IOState
 
 
 withPos :: P.MonadParsec e s m => (Position -> m a) -> m a
@@ -47,5 +46,3 @@ symbol' x = withPos $ \pos -> do
     symbol <- symbol x
     return (pos, symbol)
 
-test :: Parser Position
-test = withPos return
