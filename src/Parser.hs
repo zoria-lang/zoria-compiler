d76abc648c198d14057b015f16877041c0d04b79
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-04 04:50:40 +0200

    Helper functions for monad transformers.
    Previously it was not convenient to use the Parser type
    because you had to manually use both runStateT and runParserT.
    I've added the runParser function which combines these two.

--- src/Parser.hs
+++ src/Parser.hs
@@ -9,17 +9,17 @@ import qualified Data.Text.IO as T
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as L
-import Control.Monad.State (StateT, runStateT)
+import Control.Monad.State (StateT, runStateT, get, put)
+import Control.Monad.Trans.Class (lift)
 import Data.Void (Void)
 import Control.Monad (void, mapM_)
-import Control.Monad.IO.Class (liftIO)
 import Utility (Position(..))
-
 
 
 type ParserState = Int -- TODO: change
 type IOState = StateT ParserState IO
 type Parser = P.ParsecT Void T.Text IOState
+type Errors = P.ParseErrorBundle T.Text Void
 
 
 withPos :: P.MonadParsec e s m => (Position -> m a) -> m a
@@ -46,3 +46,31 @@ symbol' x = withPos $ \pos -> do
     symbol <- symbol x
     return (pos, symbol)
 
+liftState :: IOState a -> Parser a
+liftState = lift
+
+liftIO :: IO a -> Parser a
+liftIO = lift . lift
+
+runParser :: Parser a 
+          -> FilePath 
+          -> T.Text 
+          -> ParserState 
+          -> IO (Either Errors a)
+runParser parser file input initState = do
+    (result, _) <- runStateT state initState
+    return result
+  where
+    state = P.runParserT parser file input
+
+-- example:
+test :: Parser ()
+test = withPos $ \pos -> do
+    liftIO $ putStrLn $ show pos
+    len <- length <$> P.many "*"
+    st <- liftState get
+    liftIO $ putStrLn $ "Old value: " ++ show st ++ "\n"
+    liftState $ put (st + len)
+    st' <- liftState get
+    liftIO $ putStrLn $ "New value: " ++ show st' ++ "\n"
+    return ()
