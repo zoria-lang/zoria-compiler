5461eeb9fab9689ec51eaa889a2b4772a57968cd
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-04 03:18:04 +0200

    Ditched the lexer.
    Unfortunately the megaparsec library makes it a bit difficult to
    handle a separate lexer while keeping track of current position (the
    getSourcePos operation is supposedly slow acording to the documentation
    (i suspect it goes through the entire file to calculate line and column
    numbers based on the stored offset) and it would have to be called for
    every token).
    
    Since getSourcePos 'is not cheap' I've introduced another way to store
    positions in AST. Instead of extracting the SourcePos from the parser
    state, I only extract the file name and the offset, which is cheap.
    Then, if there is an error we can simply take the offset and find the line
    and column numbers ourselves.
    The AST types have SourcePos baked-in unfortunately so we will need
    to change it in a future pull request.

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,15 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Parser 
     () 
 where
 
-import Lexer
 import qualified Data.Text as T
-import Text.Megaparsec (Parsec, ParsecT, )
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Void (Void)
+import Control.Monad (void)
 
 
-type IOParser = ParsecT Void T.Text IO
+type IOParser = P.ParsecT Void T.Text IO
 
-type Parser = Parsec Void T.Text
+type Parser = P.Parsec Void T.Text
+
+data Position = Position
+    { posOffset :: !Int
+    , posFile   :: FilePath
+    }
+  deriving Show
 
 
+withPos :: P.MonadParsec e s m => (Position -> m a) -> m a
+withPos f = do
+    state  <- P.getParserState
+    offset <- P.getOffset
+    let file = P.sourceName . P.pstateSourcePos . P.statePosState $ state
+    f $ Position offset file 
+
+skipWhitespace :: Parser ()
+skipWhitespace = L.space (void P.spaceChar) lineComment blockComment
+  where
+    lineComment  = L.skipLineComment "#"
+    blockComment = L.skipBlockComment "#:" ":#"
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme skipWhitespace
+
+symbol :: T.Text -> Parser T.Text
+symbol = L.symbol skipWhitespace
+
+symbol' :: T.Text -> Parser (Position, T.Text)
+symbol' x = withPos $ \pos -> do
+    symbol <- symbol x
+    return (pos, symbol)
+
+test :: Parser Position
+test = withPos return
