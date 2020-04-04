5df79f2271e8a25fcec30c3b7aa5b725588f1eef
Jakub Grobelny <jgrobelny1@gmail.com>
2020-04-04 21:04:46 +0200

    Added filepath library.
    Added filepath library to obtain the </> operator for path concatenation.

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,15 +7,18 @@ import qualified Data.Text.IO as T
 import qualified Text.Megaparsec as P
 import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as L
+import System.FilePath.Posix ((</>))
 import Control.Monad.State (StateT, runStateT, get, put)
 import Control.Monad.Trans.Reader (ReaderT, runReaderT, ask)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad (forM)
 import Data.Void (Void)
 import Control.Monad (void, mapM_)
 import Utility (Position(..))
 import GetOpt (Options(..), ModulePath(..))
 import Data.Map as Map
+import Data.Set as Set
 import Syntax as AST
 
 
@@ -26,6 +29,7 @@ type Errors = P.ParseErrorBundle T.Text Void
 
 data ParserState = ParserState
     { stateOperators :: Map.Map Priority OperatorTable
+    , stateVisited   :: Set.Set ModuleId
     , stateModule    :: [ModuleHeader]
     }
   deriving Show
@@ -50,8 +54,47 @@ data OperatorTable = OpTable
     }
   deriving Show
 
+stdLibraryPath :: FilePath
+stdLibraryPath = undefined
+
+stdPrelude :: FilePath
+stdPrelude = stdLibraryPath </> "Prelude"
+
 newParserState :: ParserState
-newParserState = ParserState Map.empty []
+newParserState = ParserState Map.empty Set.empty []
+
+program :: Parser (AST.Program ())
+program = do
+    -- at the moment the rest of the input files is discarded
+    -- TODO: assure that there can be only one input file specified
+    rootFile <- head . optInputs <$> getopt
+    rootModule <- parseFile rootFile
+    return $ Program rootModule
+
+parseFile :: FilePath -> Parser (Module ())
+parseFile path = do
+    file <- liftIO $ readFile path
+    protoHeader <- moduleHeader
+    return undefined
+
+moduleHeader :: Parser (Module ())
+moduleHeader = do
+    symbol "module"
+    return undefined
+
+
+runParser :: Parser a 
+          -> FilePath
+          -> T.Text 
+          -> ParserState 
+          -> Options
+          -> IO (Either Errors a)
+runParser parser file input initState options = do
+    (result, _) <- runReaderT reader options
+    return result
+  where
+    state = P.runParserT parser file input
+    reader = runStateT state initState
 
 withPos :: P.MonadParsec e s m => (Position -> m a) -> m a
 withPos f = do
@@ -79,16 +122,3 @@ symbol' x = withPos $ \pos -> do
 
 getopt :: Parser Options
 getopt = lift . lift $ ask
-
-runParser :: Parser a 
-          -> FilePath
-          -> T.Text 
-          -> ParserState 
-          -> Options
-          -> IO (Either Errors a)
-runParser parser file input initState options = do
-    (result, _) <- runReaderT reader options
-    return result
-  where
-    state = P.runParserT parser file input
-    reader = runStateT state initState
